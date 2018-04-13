b6c1c8110b0d95f7c3bff5795649fc2c4ed6eca9
Branko Kovacevic <38045016+BKovacevic@users.noreply.github.com>
2018-04-13 18:38:26 +0200

    Add files via upload
    OOP

---
+++ BlackjackCard.java
@@ -0,0 +1,13 @@
+
+package card;
+
+
+public class BlackjackCard extends Card {
+
+    @Override
+    int get_value() {
+        return super.get_value(); 
+    }
+
+    
+}
---
+++ Card.java
@@ -0,0 +1,26 @@
+
+package card;
+
+
+public class Card {
+ 
+   int number;
+
+   String sign;
+
+   int get_value(){
+       
+       return number;
+                  
+                  }
+
+  }
+
+      
+
+   
+    
+    
+    
+    
+
